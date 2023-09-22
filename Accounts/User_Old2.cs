2fe55df0b2d29474fe7784c118a38ea4a9719239
Flo <flo@uwap.org>
2023-09-22 13:29:21 +0200

    Created class User_Old2 with all properties of User except Password2 instead of Password3 and no methods, and added constructor User(User_Old2 old). This was done so old User objects can still be deserialized and upgraded.

---
+++ Accounts/User_Old2.cs
@@ -0,0 +1,35 @@
+﻿using System.Runtime.Serialization;
+using uwap.Database;
+
+namespace uwap.WebFramework.Accounts;
+
+/// <summary>
+/// Old version of the User class to allow old objects to still be parsed and upgraded.
+/// </summary>
+[DataContract]
+public class User_Old2 : ITableValue
+{
+    [DataMember]
+    public string Id = "";
+
+    [DataMember]
+    public string _Username = "";
+
+    [DataMember]
+    public string _MailAddress = "";
+
+    [DataMember]
+    public ushort _AccessLevel = 1;
+
+    [DataMember]
+    public Password2? Password = null;
+
+    [DataMember]
+    public string? MailToken = null;
+
+    [DataMember]
+    public DateTime Signup = DateTime.UtcNow;
+
+    [DataMember]
+    public TwoFactorData? _TwoFactor = null;
+}
