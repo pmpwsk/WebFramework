de87877c57589fd098ebf06fb7bf66030745844d
Flo <flo@uwap.org>
2023-09-26 16:51:22 +0200

    Created an abstract class TextRequest to summarize stuff for certain request types.

---
+++ Requests/TextRequest.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using uwap.WebFramework.Accounts;
+
+namespace uwap.WebFramework;
+
+/// <summary>
+/// Abstract class that adds CORS and text responses to IRequest.
+/// </summary>
+public abstract class TextRequest : IRequest
+{
+    /// <summary>
+    /// Creates a new TextRequest.
+    /// </summary>
+    public TextRequest(HttpContext context, User? user, UserTable? userTable, LoginState loginState)
+        : base(context, user, userTable, loginState) { }
+}
