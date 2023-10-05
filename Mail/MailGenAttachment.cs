96f2fb446231b81d54d48ef02c3c5c75620b4549
Flo <flo@uwap.org>
2023-10-05 14:22:38 +0200

    Added class MailGenAttachment.

---
+++ Mail/MailGenAttachment.cs
@@ -0,0 +1,34 @@
+﻿using MimeKit;
+
+namespace uwap.WebFramework.Mail;
+
+/// <summary>
+/// Contains data for a mail attachment so it can be generated and sent out later.
+/// </summary>
+public class MailGenAttachment
+{
+    /// <summary>
+    /// The file's name.
+    /// </summary>
+    public string? Name;
+
+    /// <summary>
+    /// The file's content type.
+    /// </summary>
+    public string? ContentType;
+
+    /// <summary>
+    /// The bytes of the file.
+    /// </summary>
+    public byte[] Bytes;
+
+    /// <summary>
+    /// Creates a new mail attachment object.
+    /// </summary>
+    public MailGenAttachment(string path, string? name, string? contentType)
+    {
+        Bytes = File.ReadAllBytes(path);
+        Name = name;
+        ContentType = contentType;
+    }
+}
