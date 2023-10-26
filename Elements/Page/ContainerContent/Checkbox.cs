390b7d061289968ec7f79c1ac691e94996066457
Flo <flo@uwap.org>
2023-10-26 20:55:09 +0200

    Added Checkbox as IContent with string Text and bool Checked.

---
+++ Elements/Page/ContainerContent/Checkbox.cs
@@ -0,0 +1,36 @@
+﻿namespace uwap.WebFramework.Elements;
+
+/// <summary>
+/// Checkbox that can either be enabled or disabled.
+/// </summary>
+public class Checkbox : IContent
+{
+    //not needed
+    protected override string ElementType => "";
+
+    /// <summary>
+    /// The text for the checkbox.
+    /// </summary>
+    public string Text;
+
+    /// <summary>
+    /// Whether the checkbox should be checked by default.
+    /// </summary>
+    public bool Checked;
+
+    /// <summary>
+    /// Creates a new checkbox element with the given ID for a container.
+    /// </summary>
+    public Checkbox(string text, string id, bool isChecked = false)
+    {
+        Text = text;
+        Id = id;
+        Checked = isChecked;
+    }
+
+    //documentation inherited from IContent
+    public override IEnumerable<string> Export()
+    {
+        yield return $"<div><input type=\"checkbox\" id=\"{Id}\" name=\"{Id}\"{(Checked ? " checked" : "")} /><label for=\"{Id}\">{Text}</label></div>";
+    }
+}
