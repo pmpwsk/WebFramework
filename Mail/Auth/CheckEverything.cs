d793f1d38361a9f64bfae1ac6f73e4c923acb84a
Flo <flo@uwap.org>
2023-10-29 23:51:11 +0100

    Added MailAuth.CheckEverything.

---
+++ Mail/Auth/CheckEverything.cs
@@ -0,0 +1,9 @@
+﻿using MimeKit;
+
+namespace uwap.WebFramework.Mail;
+
+public static partial class MailAuth
+{
+    public static FullResult CheckEverything(MailConnectionData connectionData, MimeMessage message, List<string>? logToPopulate)
+        => new(connectionData, message, logToPopulate);
+}
