8a23e1e76c8ae54b6e70e90369aeb74eab08b3e7
Flo <flo@uwap.org>
2023-10-29 23:48:45 +0100

    Made logToPopulate of MailAuth.FullResult nullable.

--- Mail/Auth/FullResult.cs
+++ Mail/Auth/FullResult.cs
@@ -23,28 +23,34 @@ public static partial class MailAuth
         [DataMember]
         public readonly MailAuthVerdictDMARC DMARC;
 
-        internal FullResult(MailConnectionData connectionData, MimeMessage message, List<string> logToPopulate)
+        internal FullResult(MailConnectionData connectionData, MimeMessage message, List<string>? logToPopulate)
         {
             IPAddress = connectionData.IP.Address.ToString();
             Secure = connectionData.Secure;
-
-            logToPopulate.Add("From: " + IPAddress);
-            logToPopulate.Add("Secure: " + Secure.ToString());
 
             string fromDomain = message.From.Mailboxes.First().Domain;
             string? returnHeader = message.Headers[HeaderId.ReturnPath];
             string returnDomain = (returnHeader != null && MailboxAddress.TryParse(returnHeader, out var address)) ? address.Domain : fromDomain;
 
             SPF = CheckSPF(returnDomain, connectionData.IP.Address, out var spfPassedDomain);
-            logToPopulate.Add($"SPF: {SPF}{(spfPassedDomain == null ? "" : $" with {spfPassedDomain}")}");
 
             DKIM = CheckDKIM(message, out var dkimResults);
-            logToPopulate.Add($"DKIM: {DKIM}");
-            foreach (var ds in dkimResults)
-                logToPopulate.Add($"DKIM (domain={ds.Key.Domain}, selector={ds.Key.Selector}): {(ds.Value ? "Pass" : "Fail")}");
 
             DMARC = CheckDMARC(returnDomain, fromDomain, SPF, DKIM, dkimResults);
-            logToPopulate.Add($"DMARC: {DMARC}");
+
+            if (logToPopulate != null)
+            {
+                logToPopulate.Add("From: " + IPAddress);
+                logToPopulate.Add("Secure: " + Secure.ToString());
+
+                logToPopulate.Add($"SPF: {SPF}{(spfPassedDomain == null ? "" : $" with {spfPassedDomain}")}");
+
+                logToPopulate.Add($"DKIM: {DKIM}");
+                foreach (var ds in dkimResults)
+                    logToPopulate.Add($"DKIM (domain={ds.Key.Domain}, selector={ds.Key.Selector}): {(ds.Value ? "Pass" : "Fail")}");
+
+                logToPopulate.Add($"DMARC: {DMARC}");
+            }
         }
     }
 }
