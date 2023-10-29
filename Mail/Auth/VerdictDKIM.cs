d7a45efbae075425cad5030d72a3d8a03b679c1d
Flo <flo@uwap.org>
2023-10-29 23:37:34 +0100

    Added VerdictDKIM.

---
+++ Mail/Auth/VerdictDKIM.cs
@@ -0,0 +1,12 @@
+﻿namespace uwap.WebFramework.Mail;
+
+public static partial class MailAuth
+{
+    public enum MailAuthVerdictDKIM
+    {
+        Fail = -2,
+        Mixed = -1,
+        Unset = 0,
+        Pass = 1
+    }
+}
