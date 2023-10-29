9af960e6e2b5347c1577e6a013c8968b6a110005
Flo <flo@uwap.org>
2023-10-29 23:38:19 +0100

    Added DomainSelectorPair.

---
+++ Mail/Auth/DomainSelectorPair.cs
@@ -0,0 +1,15 @@
+﻿namespace uwap.WebFramework.Mail;
+
+public static partial class MailAuth
+{
+    public readonly struct DomainSelectorPair
+    {
+        public readonly string Domain, Selector;
+
+        public DomainSelectorPair(string domain, string selector)
+        {
+            Domain = domain;
+            Selector = selector;
+        }
+    }
+}
