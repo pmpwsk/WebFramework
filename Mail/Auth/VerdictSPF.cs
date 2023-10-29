4079e4138a48bd057eb4d716cd94a9165f4c520b
Flo <flo@uwap.org>
2023-10-29 23:36:46 +0100

    Added VerdictSPF.

---
+++ Mail/Auth/VerdictSPF.cs
@@ -0,0 +1,12 @@
+﻿namespace uwap.WebFramework.Mail;
+
+public static partial class MailAuth
+{
+    public enum MailAuthVerdictSPF
+    {
+        HardFail = -2,
+        SoftFail = -1,
+        Unset = 0,
+        Pass = 1
+    }
+}
