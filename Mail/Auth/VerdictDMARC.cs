9535c618c7ed0f506dd7bc51eb9c648c82fb261c
Flo <flo@uwap.org>
2023-10-29 23:37:26 +0100

    Added VerdictDMARC.

---
+++ Mail/Auth/VerdictDMARC.cs
@@ -0,0 +1,13 @@
+﻿namespace uwap.WebFramework.Mail;
+
+public static partial class MailAuth
+{
+    public enum MailAuthVerdictDMARC
+    {
+        FailWithReject = -3,
+        FailWithQuarantine = -2,
+        FailWithoutAction = -1,
+        Unset = 0,
+        Pass = 1
+    }
+}
