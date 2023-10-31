a1a9dba3df1bb442fd83919312f8f8c1404601cb
Flo <flo@uwap.org>
2023-10-31 01:14:52 +0100

    Added documentation for VerdictSPF.

--- Mail/Auth/VerdictSPF.cs
+++ Mail/Auth/VerdictSPF.cs
@@ -2,11 +2,29 @@
 
 public static partial class MailAuth
 {
+    /// <summary>
+    /// Possible results for SPF.
+    /// </summary>
     public enum MailAuthVerdictSPF
     {
+        /// <summary>
+        /// This message should be rejected.
+        /// </summary>
         HardFail = -2,
+
+        /// <summary>
+        /// This message should go to the spam folder.
+        /// </summary>
         SoftFail = -1,
+
+        /// <summary>
+        /// No valid SPF record was found for this message.
+        /// </summary>
         Unset = 0,
+
+        /// <summary>
+        /// This message passed SPF.
+        /// </summary>
         Pass = 1
     }
 }
