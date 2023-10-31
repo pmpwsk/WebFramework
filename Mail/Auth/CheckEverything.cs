c647ee3cb25eed4edbaadf871dee91eb2577bb4f
Flo <flo@uwap.org>
2023-10-31 01:13:22 +0100

    Added documentation for CheckEverything.

--- Mail/Auth/CheckEverything.cs
+++ Mail/Auth/CheckEverything.cs
@@ -4,6 +4,10 @@ namespace uwap.WebFramework.Mail;
 
 public static partial class MailAuth
 {
+    /// <summary>
+    /// Calls all of the checking methods and fills the given log list (if present) with details that aren't saved otherwise.<br/>
+    /// The MailConnectionData object is provided in the event for incoming mail messages.
+    /// </summary>
     public static FullResult CheckEverything(MailConnectionData connectionData, MimeMessage message, List<string>? logToPopulate)
         => new(connectionData, message, logToPopulate);
 }
