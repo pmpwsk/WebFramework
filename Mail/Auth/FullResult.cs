797e78c0a0814af5c8c2bd7f65d94afa9b0152ee
Flo <flo@uwap.org>
2023-10-31 01:14:12 +0100

    Added documentation for FullResult.

--- Mail/Auth/FullResult.cs
+++ Mail/Auth/FullResult.cs
@@ -5,24 +5,46 @@ namespace uwap.WebFramework.Mail;
 
 public static partial class MailAuth
 {
+    /// <summary>
+    /// Class to contain a full mail authentication evaluation (SPF, DKIM and DMARC) for a message.<br/>
+    /// Returned by CheckEverything(...).
+    /// </summary>
     [DataContract]
     public class FullResult
     {
+        /// <summary>
+        /// The IP address the message was sent from.
+        /// </summary>
         [DataMember]
         public readonly string IPAddress;
 
+        /// <summary>
+        /// Whether the message was sent over an encrypted connection.
+        /// </summary>
         [DataMember]
         public readonly bool Secure;
 
+        /// <summary>
+        /// The result of SPF checking.
+        /// </summary>
         [DataMember]
         public readonly MailAuthVerdictSPF SPF;
 
+        /// <summary>
+        /// The result of DKIM checking.
+        /// </summary>
         [DataMember]
         public readonly MailAuthVerdictDKIM DKIM;
 
+        /// <summary>
+        /// The result of DMARC checking.
+        /// </summary>
         [DataMember]
         public readonly MailAuthVerdictDMARC DMARC;
 
+        /// <summary>
+        /// Calls all of the checking methods and fills the given log list (if present) with details that aren't saved otherwise.
+        /// </summary>
         internal FullResult(MailConnectionData connectionData, MimeMessage message, List<string>? logToPopulate)
         {
             IPAddress = connectionData.IP.Address.ToString();
