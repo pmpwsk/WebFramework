5e643509d5871806a1015bdf68784dc22da4d55d
Flo <flo@uwap.org>
2023-10-31 01:13:46 +0100

    Added documentation for DmarcAlignment.

--- Mail/Auth/DmarcAlignment.cs
+++ Mail/Auth/DmarcAlignment.cs
@@ -2,9 +2,19 @@
 
 public static partial class MailAuth
 {
+    /// <summary>
+    /// Possible alignments for SPF and DKIM in DMARC.
+    /// </summary>
     public enum DmarcAlignment
     {
+        /// <summary>
+        /// The two domains to be compared must either be the same or one must be a subdomain of the other one (both directions are okay).
+        /// </summary>
         Relaxed,
+
+        /// <summary>
+        /// The two domains to be compared must be exactly the same.
+        /// </summary>
         Strict
     }
 }
