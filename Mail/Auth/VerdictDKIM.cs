38f81ffc10989c89ec0b14902db73e75a06a9538
Flo <flo@uwap.org>
2023-10-31 01:14:27 +0100

    Added documentation for VerdictDKIM.

--- Mail/Auth/VerdictDKIM.cs
+++ Mail/Auth/VerdictDKIM.cs
@@ -2,11 +2,29 @@
 
 public static partial class MailAuth
 {
+    /// <summary>
+    /// Possible results for DKIM.
+    /// </summary>
     public enum MailAuthVerdictDKIM
     {
+        /// <summary>
+        /// All of the signatures are invalid.
+        /// </summary>
         Fail = -2,
+
+        /// <summary>
+        /// Some of the signatures are invalid, but some are valid.
+        /// </summary>
         Mixed = -1,
+
+        /// <summary>
+        /// No signatures found.
+        /// </summary>
         Unset = 0,
+
+        /// <summary>
+        /// All of the signatures are valid.
+        /// </summary>
         Pass = 1
     }
 }
