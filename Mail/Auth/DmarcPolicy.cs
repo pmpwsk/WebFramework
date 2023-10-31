623e84660b3f60a2ef311a3796741b9df9c01b56
Flo <flo@uwap.org>
2023-10-31 01:13:54 +0100

    Added documentation for DmarcPolicy.

--- Mail/Auth/DmarcPolicy.cs
+++ Mail/Auth/DmarcPolicy.cs
@@ -2,10 +2,24 @@
 
 public static partial class MailAuth
 {
+    /// <summary>
+    /// Possible policies for DMARC.
+    /// </summary>
     private enum DmarcPolicy
     {
+        /// <summary>
+        /// DMARC failures should be ignored.
+        /// </summary>
         None,
+
+        /// <summary>
+        /// DMARC failures should cause the failing messages to go to the spam folder.
+        /// </summary>
         Quarantine,
+
+        /// <summary>
+        /// DMARC failures should cause the failing messages to be rejected.
+        /// </summary>
         Reject
     }
 }
