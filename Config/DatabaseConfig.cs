c0b1b6e3f705aa45858824ad002e9d32a4629e4f
Flo <flo@uwap.org>
2023-11-17 00:43:27 +0100

    Moved Server.Config.Database into a separate file.

---
+++ Config/DatabaseConfig.cs
@@ -0,0 +1,19 @@
+﻿namespace uwap.WebFramework;
+
+public static partial class Server
+{
+    public static partial class Config
+    {
+        /// <summary>
+        /// Settings to control the database.
+        /// </summary>
+        public static class Database
+        {
+            /// <summary>
+            /// Whether to write the new JSON to the disk if it doesn't match the old one while loading a table.<br/>
+            /// Default: true
+            /// </summary>
+            public static bool WriteBackOnLoad { get; set; } = true;
+        }
+    }
+}
--- Server/Config.cs
+++ Server/Config.cs
@@ -7,16 +7,6 @@ public static partial class Server
     /// </summary>
     public static partial class Config
     {
-        /// <summary>
-        /// Settings to control the database.
-        /// </summary>
-        public static class Database
-        {
-            /// <summary>
-            /// Whether to write the new JSON to the disk if it doesn't match the old one while loading a table.<br/>
-            /// Default: true
-            /// </summary>
-            public static bool WriteBackOnLoad { get; set; } = true;
-        }
+        
     }
 }
