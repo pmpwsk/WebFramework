2955550f91b2ff5bf943afba6953caec4558f4d6
Flo <flo@uwap.org>
2023-11-18 14:05:10 +0100

    Added CheckPTR.

---
+++ Mail/Auth/CheckPTR.cs
@@ -0,0 +1,17 @@
+﻿using System.Net;
+
+namespace uwap.WebFramework.Mail;
+
+public static partial class MailAuth
+{
+    /// <summary>
+    /// Checks the given IP address for a PTR record.
+    /// </summary>
+    public static string? CheckPTR(IPAddress ip)
+    {
+        var query = MailManager.DnsLookup.QueryReverse(ip);
+        var records = query.Answers.PtrRecords();
+        var record = records.FirstOrDefault();
+        return record?.PtrDomainName?.Original.TrimEnd('.');
+    }
+}
