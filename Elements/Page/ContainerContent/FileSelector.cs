61d8f2660d0aa3e927ce62f08da7f22b56ff7cd2
Flo <flo@uwap.org>
2023-11-24 18:28:41 +0100

    Added bool FileSelector.AllowMultiple.

--- Elements/Page/ContainerContent/FileSelector.cs
+++ Elements/Page/ContainerContent/FileSelector.cs
@@ -7,18 +7,24 @@ public class FileSelector : IContent
 {
     //not needed
     protected override string ElementType => "";
+
+    /// <summary>
+    /// Whether to allow the selection of multiple files.
+    /// </summary>
+    public bool AllowMultiple;
     
     /// <summary>
     /// Creates a new file selector element with the given ID for a container.
     /// </summary>
-    public FileSelector(string id)
+    public FileSelector(string id, bool allowMultiple = false)
     {
         Id = id;
+        AllowMultiple = allowMultiple;
     }
 
     //documentation inherited from IContent
     public override IEnumerable<string> Export()
     {
-        yield return $"<input type=\"file\" id=\"{Id}\">";
+        yield return $"<input type=\"file\" id=\"{Id}\"{(AllowMultiple ? " multiple" : "")}>";
     }
 }
