03d4fb84776d23b6ef9029a5064968ad4c762767
Flo <flo@uwap.org>
2023-12-03 01:07:48 +0100

    Added string? OnChange to FileSelector.

--- Elements/Page/ContainerContent/FileSelector.cs
+++ Elements/Page/ContainerContent/FileSelector.cs
@@ -12,7 +12,12 @@ public class FileSelector : IContent
     /// Whether to allow the selection of multiple files.
     /// </summary>
     public bool AllowMultiple;
-    
+
+    /// <summary>
+    /// JavaScript command that should be executed when the value changes.
+    /// </summary>
+    public string? OnChange = null;
+
     /// <summary>
     /// Creates a new file selector element with the given ID for a container.
     /// </summary>
@@ -25,6 +30,6 @@ public class FileSelector : IContent
     //documentation inherited from IContent
     public override IEnumerable<string> Export()
     {
-        yield return $"<input type=\"file\" id=\"{Id}\"{(AllowMultiple ? " multiple" : "")}>";
+        yield return $"<input type=\"file\" id=\"{Id}\"{(AllowMultiple ? " multiple" : "")}{(OnChange == null ? null : $" onchange=\"{OnChange}\"")}>";
     }
 }
