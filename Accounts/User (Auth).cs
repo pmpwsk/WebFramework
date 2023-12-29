4d0987e5c4e469100ca038f14ef79fdd710c588c
Flo <flo@uwap.org>
2023-12-29 20:41:55 +0100

    Removed unnecessary usings.

--- Accounts/User (Auth).cs
+++ Accounts/User (Auth).cs
@@ -3,7 +3,6 @@ using System.Collections.ObjectModel;
 using System.Diagnostics.CodeAnalysis;
 using System.Runtime.Serialization;
 using uwap.Database;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace uwap.WebFramework.Accounts;
 
--- Accounts/UserTable.cs
+++ Accounts/UserTable.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Configuration;
 using uwap.Database;
 
 namespace uwap.WebFramework.Accounts;
