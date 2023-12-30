1fa2f87401f9473b886dbefd9385ae9f82feb5cc
Flo <flo@uwap.org>
2023-12-30 17:14:50 +0100

    Removed HTML safety from ContainerElement titles.

--- Elements/Page/ContainerElement.cs
+++ Elements/Page/ContainerElement.cs
@@ -78,8 +78,8 @@ public class ContainerElement : IContainerElement
 
         if (Title != null)
         {
-            if (Large) yield return $"\t<h1>{Title.HtmlSafe()}</h1>";
-            else yield return $"\t<h2>{Title.HtmlSafe()}</h2>";
+            if (Large) yield return $"\t<h1>{Title}</h1>";
+            else yield return $"\t<h2>{Title}</h2>";
         }
 
         if (Buttons.Count != 0)
