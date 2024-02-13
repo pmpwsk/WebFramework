61188f0b2aea992a4cced255bb23ebc2656c4149
Flo <flo@uwap.org>
2024-02-13 05:11:19 +0100

    Added void Backup to IPlugin.

--- Plugins/IPlugin.cs
+++ Plugins/IPlugin.cs
@@ -1,4 +1,6 @@
-﻿namespace uwap.WebFramework.Plugins;
+﻿using System.Collections.ObjectModel;
+
+namespace uwap.WebFramework.Plugins;
 
 /// <summary>
 /// Interface to create plugins for request handling and a worker method.<br/>
@@ -52,4 +54,11 @@ public interface IPlugin
     /// This should return a different value every time the file is changed (but never when it hasn't been modified) and shouldn't be long (DateTime.Ticks is the intended value).
     /// </summary>
     public string? GetFileVersion(string relPath);
+
+    /// <summary>
+    /// Backs up the plugin's files and database. Make sure to read the documentation of all of the parameters as well as the guide for WF backups on uwap.org.
+    /// </summary>
+    /// <param name="id">The ID of the current backup being created, its folder (for all tables and plugins!) is [directory][id].</param>
+    /// <param name="basedOnIds">The IDs of the previous backups this backup should be based on, starting with the first one, each next one is based on the previous one and this backup should be based on the last one.</param>
+    public void Backup(string id, ReadOnlyCollection<string> basedOnIds);
 }
