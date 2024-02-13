fd8d11a4db7debbf670fc261dd73e6dd6275a816
Flo <flo@uwap.org>
2024-02-13 05:04:02 +0100

    Added BackupConfig.cs with bool Enabled.

---
+++ Config/BackupConfig.cs
@@ -0,0 +1,19 @@
+﻿namespace uwap.WebFramework;
+
+public static partial class Server
+{
+    public static partial class Config
+    {
+        /// <summary>
+        /// Settings to control backups.
+        /// </summary>
+        public static class Backup
+        {
+            /// <summary>
+            /// Whether to run automatic backups.<br/>
+            /// Default: false
+            /// </summary>
+            public static bool Enabled { get; set; } = false;
+        }
+    }
+}
