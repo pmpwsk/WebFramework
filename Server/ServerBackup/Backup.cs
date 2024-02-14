a47547fe526992a055841f80670bf3ddc7ae755b
Flo <flo@uwap.org>
2024-02-14 01:19:09 +0100

    Added a check for Config.Backup.Enabled to Backup().

--- Server/ServerBackup/Backup.cs
+++ Server/ServerBackup/Backup.cs
@@ -5,5 +5,7 @@ public static partial class Server
     private static void Backup()
     private static async Task Backup()
     {
+        if (!Config.Backup.Enabled)
+            return;
     }
 }
