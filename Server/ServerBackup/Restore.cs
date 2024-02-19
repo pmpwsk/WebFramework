4a061ab7bb93fe360f1ef35b54c85e0aabfd8557
Flo <flo@uwap.org>
2024-02-20 00:52:04 +0100

    Added code to find the IDs to Restore.

--- Server/ServerBackup/Restore.cs
+++ Server/ServerBackup/Restore.cs
@@ -1,4 +1,7 @@
+﻿using System.Collections.ObjectModel;
 ﻿namespace uwap.WebFramework;
+
+namespace uwap.WebFramework;
 
 public static partial class Server
 {
@@ -11,6 +14,23 @@ public static partial class Server
             throw new Exception("A restore is already running!");
         RestoreRunning = true;
 
+        //find ids
+        ReadOnlyCollection<string>? ids = null;
+        string lastInChain = id;
+        List<string> idsWritable = [];
+        while (File.Exists($"{Config.Backup.Directory}{lastInChain}/BasedOn.txt"))
+        {
+            idsWritable.Insert(0, lastInChain);
+            lastInChain = File.ReadAllText($"{Config.Backup.Directory}{lastInChain}/BasedOn.txt");
+            if (lastInChain == "-")
+            {
+                //start of the chain has been reached
+                ids = idsWritable.AsReadOnly();
+                break;
+            }
+        }
+        if (ids == null)
+            throw new Exception("The chain of backups is corrupt!");
         RestoreRunning = false;
     }
 }
