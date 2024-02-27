2059cfe2105f6ba785b1139f23bfb79b2c6f2cd7
Flo <flo@uwap.org>
2024-02-27 14:09:11 +0100

    Replaced the constructor of ApiRequest with a primary constructor with the only parameter being a LayerRequestData object.

--- Requests/ApiRequest.cs
+++ Requests/ApiRequest.cs
@@ -1,17 +1,8 @@
-using Microsoft.AspNetCore.Http;
-using uwap.WebFramework.Accounts;
-
 namespace uwap.WebFramework;
 
 /// <summary>
 /// Intended for backend requests, either to path /api/... or routed over an app request.
 /// </summary>
-public class ApiRequest : SimpleResponseRequest
+public class ApiRequest(LayerRequestData data) : SimpleResponseRequest(data)
 {
-    /// <summary>
-    /// Creates a new API request object with the given context, user, user table and login state.
-    /// </summary>
-    public ApiRequest(HttpContext context, User? user, UserTable? userTable, LoginState loginState) : base(context, user, userTable, loginState)
-    {
-    }
 }
