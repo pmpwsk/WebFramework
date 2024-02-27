176ddec48a0294aa3b5cf039644e452ab298876c
Flo <flo@uwap.org>
2024-02-27 14:15:09 +0100

    Added LayerDelegate.

---
+++ Server/Layers/LayerDelegate.cs
@@ -0,0 +1,6 @@
+﻿namespace uwap.WebFramework;
+
+/// <summary>
+/// Delegate for a layer of the middleware, returns whether the request has been finished (the middleware will not continue if true was returned).
+/// </summary>
+public delegate Task<bool> LayerDelegate(LayerRequestData data);
