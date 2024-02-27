61aa986acb072b7a7c65ada83dff894044f20456
Flo <flo@uwap.org>
2024-02-27 14:16:07 +0100

    Added RedirectLayer.

---
+++ Server/Layers/RedirectLayer.cs
@@ -0,0 +1,21 @@
+﻿namespace uwap.WebFramework;
+
+public static partial class Server
+{
+    public static partial class Layers
+    {
+        /// <summary>
+        /// Redirects if there's a matching entry in Config.Domains.Redirect.
+        /// </summary>
+        public static Task<bool> RedirectLayer(LayerRequestData data)
+        {
+            if (Config.Domains.Redirect.TryGetValue(data.Context.Domain(), out string? redirectTarget))
+            {
+                data.Redirect(data.Context.Proto() + redirectTarget + data.Path + data.Context.Query(), true);
+                return Task.FromResult(true);
+            }
+
+            return Task.FromResult(false);
+        }
+    }
+}
