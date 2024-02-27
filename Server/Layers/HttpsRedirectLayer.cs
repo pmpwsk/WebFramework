5f7e32816ec23a19f884e71d2eddff067b9cc48e
Flo <flo@uwap.org>
2024-02-27 14:15:40 +0100

    Added HttpsRedirectLayer.

---
+++ Server/Layers/HttpsRedirectLayer.cs
@@ -0,0 +1,21 @@
+﻿namespace uwap.WebFramework;
+
+public static partial class Server
+{
+    public static partial class Layers
+    {
+        /// <summary>
+        /// Redirects to HTTPS if necessary and possible.
+        /// </summary>
+        public static Task<bool> HttpsRedirectLayer(LayerRequestData data)
+        {
+            if (Config.HttpsPort != null && !data.Context.Request.IsHttps)
+            {
+                data.Context.Response.Redirect($"https://{data.Domain}{(Config.HttpsPort == 443 ? "" : $":{Config.HttpsPort}")}{data.Path}{data.Context.Request.QueryString}", true);
+                return Task.FromResult(true);
+            }
+
+            return Task.FromResult(false);
+        }
+    }
+}
