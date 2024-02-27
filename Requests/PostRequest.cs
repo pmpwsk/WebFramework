ed16e670191ed37cb8690e90f207f3b429af1cdc
Flo <flo@uwap.org>
2024-02-27 14:11:12 +0100

    Replaced the constructor of PostRequest with a primary constructor with the only parameter being a LayerRequestData object.

--- Requests/PostRequest.cs
+++ Requests/PostRequest.cs
@@ -1,20 +1,12 @@
 using Microsoft.AspNetCore.Http;
-using uwap.WebFramework.Accounts;
 
 namespace uwap.WebFramework;
 
 /// <summary>
 /// Intended for POST requests without files.
 /// </summary>
-public class PostRequest : SimpleResponseRequest
+public class PostRequest(LayerRequestData data) : SimpleResponseRequest(data)
 {
-    /// <summary>
-    /// Creates a new POST request object with the given context, user, user table and login state.
-    /// </summary>
-    public PostRequest(HttpContext context, User? user, UserTable? userTable, LoginState loginState) : base(context, user, userTable, loginState)
-    {
-    }
-
     /// <summary>
     /// Whether the request has set a content type for a form.
     /// </summary>
