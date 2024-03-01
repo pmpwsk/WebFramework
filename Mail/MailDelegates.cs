7582477d13851bed49ff8af36e6e5529ee728f9a
Flo <flo@uwap.org>
2024-03-01 22:20:46 +0100

    Replaced MailboxFilterResult AcceptMailDelegate with bool MailboxExistsDelegate.

--- Mail/MailDelegates.cs
+++ Mail/MailDelegates.cs
@@ -1,6 +1,5 @@
 ﻿using MimeKit;
 using SmtpServer.Mail;
-using SmtpServer.Storage;
 using SmtpServer;
 using SmtpServer.Protocol;
 using System.Diagnostics.CodeAnalysis;
@@ -15,7 +14,7 @@ public delegate void SentDelegate(MimeMessage message, MailSendResult result);
 /// <summary>
 /// A delegate that is used for methods that decide whether a mail message with the given information should be accepted or not.
 /// </summary>
-public delegate MailboxFilterResult AcceptDelegate(ISessionContext context, IMailbox from, IMailbox to);
+public delegate bool MailboxExistsDelegate(ISessionContext context, IMailbox from, IMailbox to);
 
 /// <summary>
 /// A delegate that is used for methods that handle given mail messages (along with the given mail context and authentication result) after they have been accepted by an accepting method.
