9f2509d36c70b911cb4548db66aad45021a1e310
pmpwsk <flo@uwap.org>
2024-06-23 23:22:24 +0200

    Removed code limiting paths to the mail verification or 2FA path if either one is required.

--- Server/Layers/AuthLayer.cs
+++ Server/Layers/AuthLayer.cs
@@ -25,43 +25,12 @@ public static partial class Server
                 data.LoginState = LoginState.None;
             }
 
-
             string fullPath = data.Domain + data.Path;
             if (limitedToPaths != null && !limitedToPaths.Any(x => x == fullPath || (x.EndsWith('*') && fullPath.StartsWith(x[..^1]))))
             {
                 data.Status = 403;
                 return Task.FromResult(true);
             }
-
-
-            if (data.Path.StartsWith("/api/") || Config.Accounts.LoginAllowedPaths == null || Config.Accounts.LoginAllowedPaths.Contains(data.Path))
-            {
-            }
-            else if (data.LoginState == LoginState.NeedsMailVerification)
-            {
-                if (data.Path != Config.Accounts.MailVerifyPath)
-                {
-                    IPlugin? plugin = PluginManager.GetPlugin(data.Domains, data.Path, out string relPath, out _, out _);
-                    if (plugin == null || plugin.GetFileVersion(relPath) == null)
-                    {
-                        data.Redirect($"{Config.Accounts.MailVerifyPath}?redirect={HttpUtility.UrlEncode(data.Context.PathQuery())}");
-                        return Task.FromResult(true);
-                    }
-                }
-            }
-            else if (data.LoginState == LoginState.Needs2FA)
-            {
-                if (data.Path != Config.Accounts.TwoFactorPath)
-                {
-                    IPlugin? plugin = PluginManager.GetPlugin(data.Domains, data.Path, out string relPath, out _, out _);
-                    if (plugin == null || plugin.GetFileVersion(relPath) == null)
-                    {
-                        data.Redirect($"{Config.Accounts.TwoFactorPath}?redirect={HttpUtility.UrlEncode(data.Context.PathQuery())}");
-                        return Task.FromResult(true);
-                    }
-                }
-            }
-
 
             return Task.FromResult(false);
         }
