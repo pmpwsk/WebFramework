45dfd6a0245e79ac64ee486cbcd0ee74f3f31aac
flo <flo@uwap.org>
2024-07-16 11:13:24 +0200

    Added CancellationToken Server.StoppingToken (along with StoppingTokenSource and LifetimeService to make it work).

---
+++ Server/LifetimeService.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace uwap.WebFramework;
+
+/// <summary>
+/// Main class to manage the WebFramework server.
+/// </summary>
+public static partial class Server
+{
+    private static readonly CancellationTokenSource StoppingTokenSource = new();
+
+    public static readonly CancellationToken StoppingToken = StoppingTokenSource.Token;
+
+    private class LifetimeService(IHostApplicationLifetime hal) : IHostedService
+    {
+        private readonly IHostApplicationLifetime HostApplicationLifetime = hal;
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            HostApplicationLifetime.ApplicationStopping.Register(ApplicationStopping);
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+            => Task.CompletedTask;
+
+        private void ApplicationStopping()
+        {
+            Console.WriteLine("Stopping...");
+            PauseRequests = true;
+            StoppingTokenSource.Cancel();
+        }
+    }
+}
