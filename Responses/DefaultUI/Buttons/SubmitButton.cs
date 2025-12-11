d1ee1e08c5efd90d9d9c486487e14d737c92cf84
pmpwsk <flo@uwap.org>
2025-12-11 18:33:02 +0100

    Fixed the submit button's text.

--- Responses/DefaultUI/Buttons/SubmitButton.cs
+++ Responses/DefaultUI/Buttons/SubmitButton.cs
@@ -5,13 +5,16 @@ namespace uwap.WebFramework.Responses.DefaultUI;
 /// <summary>
 /// A default UI button that submits a form.
 /// </summary>
-public class SubmitButton : ButtonWithText
+public class SubmitButton : AbstractButton
 {
     private readonly OptionalWatchedAttribute OverrideActionAttribute;
     
-    public SubmitButton(string text, string? overrideAction = null) : base(text)
+    private readonly RequiredWatchedAttribute TextAttribute;
+    
+    public SubmitButton(string text, string? overrideAction = null)
     {
         OverrideActionAttribute = new(this, "formaction", overrideAction);
+        TextAttribute = new(this, "value", text);
     }
     
     public override string RenderedTag
@@ -25,16 +28,27 @@ public class SubmitButton : ButtonWithText
         get => OverrideActionAttribute.Value;
         set => OverrideActionAttribute.Value = value;
     }
-
-    public override IEnumerable<(string Name, string? Value)> FixedAttributes
-        => [
-            ..base.FixedAttributes,
-            ("type", "submit")
-        ];
+    
+    /// <summary>
+    /// The button's text.
+    /// </summary>
+    public string Text
+    {
+        get => TextAttribute.Value;
+        set => TextAttribute.Value = value;
+    }
 
     public override IEnumerable<AbstractWatchedAttribute> WatchedAttributes
         => [
             ..base.WatchedAttributes,
-            OverrideActionAttribute
+            OverrideActionAttribute,
+            TextAttribute
+        ];
+
+    public override IEnumerable<(string Name, string? Value)> FixedAttributes
+        => [
+            ..base.FixedAttributes,
+            ("class", "wf-button"),
+            ("type", "submit")
         ];
 }
