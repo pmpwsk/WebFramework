9563ed023f237330bff03ad21a8ca80b08c305c8
pmpwsk <flo@uwap.org>
2025-12-19 19:13:00 +0100

    Added a container button type.

---
+++ Responses/DefaultUI/Buttons/BigLinkButton.cs
@@ -0,0 +1,53 @@
+using uwap.WebFramework.Responses.Base;
+using uwap.WebFramework.Responses.Dynamic;
+
+namespace uwap.WebFramework.Responses.DefaultUI;
+
+/// <summary>
+/// A default UI button that navigates to a URL.
+/// </summary>
+public class BigLinkButton : OptionalIdElement
+{
+    private readonly RequiredWatchedAttribute TargetAttribute;
+    
+    /// <summary>
+    /// The object describing the button's header text.
+    /// </summary>
+    private readonly RequiredWatchedContainer<Heading3> HeaderContainer;
+    
+    /// <summary>
+    /// The object describing the button's header text.
+    /// </summary>
+    public readonly ListWatchedContainer<Paragraph> Paragraphs;
+    
+    public BigLinkButton(string text, IEnumerable<string> subtexts, string target)
+    {
+        TargetAttribute = new(this, "href", target);
+        var header = new Heading3(text);
+        header.FixedAttributes.Remove(("class", "wf-heading"));
+        HeaderContainer = new(this, header);
+        Paragraphs = new(this, subtexts.Select(subtext => new Paragraph(subtext)));
+        FixedAttributes.Add(("class", "wf-button wf-button-is-container"));
+    }
+    
+    /// <summary>
+    /// The button's text.
+    /// </summary>
+    public string Text
+    {
+        get => HeaderContainer.Element.Text;
+        set => HeaderContainer.Element.Text = value;
+    }
+    
+    /// <summary>
+    /// The URL to navigate to.
+    /// </summary>
+    public string Target
+    {
+        get => TargetAttribute.Value;
+        set => TargetAttribute.Value = value;
+    }
+    
+    public override string RenderedTag
+        => "a";
+}
